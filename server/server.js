43a919e707fd919bb6308cc0887814f72167f356
JINGYIDING <dingjy94@gmail.com>
2018-08-04 15:26:46 -0400

    GET /todos

--- server/server.js
+++ server/server.js
@@ -9,6 +9,14 @@ const app = express();
 
 app.use(bodyParser.json());
 
+app.get('/todos', (req, res) => {
+  Todo.find().then(todos => {
+    res.send({todos});
+  }).catch(err => {
+    res.status(400).send(err);
+  });
+});
+
 app.post('/todos', (req, res) => {
   const todo = new Todo({
     text: req.body.text
@@ -24,3 +32,5 @@ app.post('/todos', (req, res) => {
 app.listen(3000, () => {
   console.log('listen to port 3000');
 });
+
+module.exports = {app};
