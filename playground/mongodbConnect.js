a5f36b9563a17f5de7c67bd5c7934eff9fe15273
JINGYIDING <dingjy94@gmail.com>
2018-08-03 23:34:04 -0400

    init todo project

---
+++ playground/mongodbConnect.js
@@ -0,0 +1,33 @@
+const MongoClient = require('mongodb').MongoClient;
+
+MongoClient.connect('mongodb://127.0.0.1:27017/TodoApp', (err, db) => {
+  if (err) {
+    return console.log('Unable to connect to MongoDB', err);
+  }
+  console.log('Connected to MongoDB successfully');
+
+  // db.collection('Todos').insertOne({
+  //   text: 'Something to do',
+  //   completed: false
+  // }, (err, result) => {
+  //   if (err) {
+  //     return console.log('Unable to insert todo', err);
+  //   }
+
+  //   console.log(JSON.stringify(result.ops, undefined, 2));
+  // });
+
+  db.collection('Users').insertOne({
+    name: 'Jingyi',
+    age: 24,
+    location: 'Florida'
+  }, (err, result) => {
+    if (err) {
+      return console.log('Unable to insert users', err);
+    }
+
+    console.log(result.ops);
+  });
+
+  db.close();
+});
