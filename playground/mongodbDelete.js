e11a9683824e89a0a5156a122c08eb8ec5222dbd
JINGYIDING <dingjy94@gmail.com>
2018-08-04 00:50:57 -0400

    update and delete

--- playground/mongodbDelete.js
+++ playground/mongodbDelete.js
@@ -7,12 +7,15 @@ MongoClient.connect('mongodb://127.0.0.1:27017/TodoApp', (err, db) => {
   }
   console.log('Connected to MongoDB successfully');
 
-  db.collection('Todos').find({completed: false}).toArray().then((docs) => {
-    console.log('Todos');
-    console.log(JSON.stringify(docs, undefined, 2));
-  }, (err) => {
-    console.log('Unable to fetch data', err);
+  //deleteMany
+  // db.collection('Todos').deleteMany({text: 'dinner'}).then((result) => {
+  //   console.log(result);
+  // });
+
+  //findOneAndDelete
+  db.collection('Todos').findOneAndDelete({completed: true}).then((result) => {
+    console.log(result);
   });
-  
+
   db.close();
 });
--- playground/mongodbUpdate.js
+++ playground/mongodbUpdate.js
@@ -7,12 +7,20 @@ MongoClient.connect('mongodb://127.0.0.1:27017/TodoApp', (err, db) => {
   }
   console.log('Connected to MongoDB successfully');
 
-  db.collection('Todos').find({completed: false}).toArray().then((docs) => {
-    console.log('Todos');
-    console.log(JSON.stringify(docs, undefined, 2));
-  }, (err) => {
-    console.log('Unable to fetch data', err);
+  db.collection('Users').findOneAndUpdate({ 
+    _id: new ObjectID('5b650220d601bb9d868e8a57')
+  }, {
+    $set: {
+      name: 'Ling'
+    }, 
+    $inc: {
+      age: -3
+    }
+  }, {
+    returnOriginal: false
+  }).then(result => {
+    console.log(result);
   });
-  
+
   db.close();
 });
